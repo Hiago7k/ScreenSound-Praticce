f6f2fd4fcb57d77e39a9c566a5fe7d2a8b424112
Hiago 7k <hiago7k@gmail.com>
2026-02-24 20:31:43 -0300

    criando menu de dicionario

---
+++ ScreenSound/Menus/Menu.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ScreenSound.Menus;
+
+internal class Menu
+{
+
+    // Esse metodo faz parte de CLASSE MENU
+    public void ExibirTituloDaOpcao(string titulo)
+    {
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
+}
--- ScreenSound/Modelos/Album.cs
+++ ScreenSound/Modelos/Album.cs
@@ -3,15 +3,18 @@
 internal class Album
 {
     private List<Musica> musicas = new List<Musica>();
+    private List<Album> albuns = new List<Album>();
 
     public Album(string nome)
     {
         Nome = nome;
+        ContadorDeObjetos++; // incrementar objetos automaticos para contador
     }
 
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+    public static int ContadorDeObjetos = 0;
 
     public void AdicionarMusica(Musica musica)
     {
@@ -26,5 +29,5 @@ internal class Album
             Console.WriteLine($"Música: {musica.Nome}");
         }
         Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
-    }
+    } 
 }
