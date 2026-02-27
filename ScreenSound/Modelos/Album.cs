eb031b2e10b9b96bdf4e220d692e7076f2460285
Hiago 7k <hiago7k@gmail.com>
2026-02-26 21:31:12 -0300

    implemtando metodo avaliar album

--- ScreenSound/Modelos/Album.cs
+++ ScreenSound/Modelos/Album.cs
@@ -1,10 +1,10 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Album
+internal class Album : IAvaliavel // Implementando Iterface
 {
     private List<Musica> musicas = new List<Musica>();
-    private List<Album> albuns = new List<Album>();
-
+    //private List<Album> albuns = new List<Album>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
     public Album(string nome)
     {
         Nome = nome;
@@ -14,12 +14,29 @@ internal class Album
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+
+    public double Media // Adicionando as regras necessarias
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        }
+    }
+       
+        
     public static int ContadorDeObjetos = 0;
 
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota); // Adicionando regra da interface
+    }
+
 
     public void ExibirMusicasDoAlbum()
     {
@@ -29,5 +46,7 @@ internal class Album
             Console.WriteLine($"Música: {musica.Nome}");
         }
         Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
-    } 
+    }
+
+   
 }
---
+++ ScreenSound/Modelos/IAvaliavel.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http.Headers;
+
+namespace ScreenSound.Modelos;
+
+internal interface IAvaliavel
+{
+    void AdicionarNota(Avaliacao nota);
+    double Media { get; } 
+}
